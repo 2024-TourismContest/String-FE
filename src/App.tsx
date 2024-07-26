80958700fc3bdb98590868ab91b0f8c3377df287
dani001024 <77667199+dani001024@users.noreply.github.com>
2024-07-27 03:52:18 +0900

    Update App.tsx
    APP.js import 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { RecoilRoot } from "recoil";
 import "./App.css";
 import Layout from "./components/layout/Layout";
-import Carousel from "./components/carousel";
 
 function App() {
   return (
     <RecoilRoot>
       <Layout></Layout>
-      <Carousel />
     </RecoilRoot>
   );
 }
